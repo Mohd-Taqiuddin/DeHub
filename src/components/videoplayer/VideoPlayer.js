3b1220114913ae537439233ffcf1c5463af71b36
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2022-11-08 02:27:49 -0600

    Update VideoPlayer.js

--- src/components/videoplayer/VideoPlayer.js
+++ src/components/videoplayer/VideoPlayer.js
@@ -49,7 +49,7 @@ export default function VideoPlayer({ account, contractData }) {
               style={{ maxheight: "720px" }}
             >
               <video
-                src={`https://w3s.link/ipfs/${currentHash}`}
+                src={`https://w3s.link/ipfs/${hash}`}
                 controls
               ></video>
             </div>
@@ -67,7 +67,7 @@ export default function VideoPlayer({ account, contractData }) {
                 <p>
                   <b>IPFS CID: </b>
                   <span>
-                    {currentHash}
+                    {hash}
                   </span>
                 </p>
                 <p>
